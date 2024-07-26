0d323d886a020bc0b9ec45c96b597cbef282f580
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-07-26 10:36:26 +0300

    fix: username should be unique

--- create.sql
+++ create.sql
@@ -11,7 +11,7 @@ CREATE TABLE files (
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
